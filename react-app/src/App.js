c0916f6d05f40e5bfe41008158e8eec69eb40991
mofopeadegoke <mofopeadegoke@gmail.com>
2023-11-07 21:34:41 +0200

    Better changes

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef } from "react";
 import { gsap } from "gsap";
-import ScrollTrigger from "gsap/ScrollTrigger";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import carThree from "./assets/car 3.jpg";
 import carFour from "./assets/car 4.jpg";
 import carFive from "./assets/car 5.jpg";
@@ -8,67 +8,61 @@ import "./App.css";
 
 gsap.registerPlugin(ScrollTrigger);
 
-function App() {
-  const racesRef = useRef(null);
+const App = () => {
+  const sliderRef = useRef(null);
 
   useEffect(() => {
-    const races = racesRef.current;
+    const slider = sliderRef.current;
+    const images = slider.querySelectorAll("img");
 
-    function getScrollAmount() {
-      const racesWidth = races.scrollWidth;
-      const windowWidth = window.innerWidth;
-      return -(racesWidth - windowWidth);
-    }
+    images.forEach((image, index) => {
+      ScrollTrigger.create({
+        trigger: image,
+        start: "center center",
+        end: "center center",
+        toggleClass: "active",
+        markers: true, // Remove in production
+      });
 
-    const tween = gsap.to(races, {
-      x: getScrollAmount,
-      duration: () => {
-        return Math.abs(getScrollAmount()) / 100; // Adjust the divisor for desired scroll speed
-      },
-      ease: "none",
-    });
+      gsap.to(image, {
+        scale: 1.2, // Scale up the active image
+        scrollTrigger: {
+          trigger: image,
+          start: "top center",
+          end: "bottom center",
+          scrub: 1,
+          pin: slider,
+        },
+      });
 
-    ScrollTrigger.create({
-      trigger: ".racesWrapper",
-      start: "top 20%",
-      end: () => `+=${getScrollAmount() * -1}`,
-      pin: true,
-      animation: tween,
-      scrub: 1,
-      invalidateOnRefresh: true,
+      gsap.to(image, {
+        xPercent: -100 * index,
+        scrollTrigger: {
+          trigger: slider,
+          start: "top top",
+          end: () => `+=${100 * (images.length - 1)}`,
+          scrub: 1,
+          pin: slider,
+        },
+      });
     });
   }, []);
 
   return (
-    <div>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium
-        quia ipsum ullam facere dignissimos quos quisquam. Pariatur excepturi
-        eveniet error sint similique. Cupiditate exercitationem quaerat harum
-        cumque fuga commodi ipsum nulla, tempora dignissimos error voluptates
-        nobis praesentium pariatur explicabo possimus?
-      </p>
-      <div className="racesWrapper">
-        <div className="races" ref={racesRef}>
-          <img src={carThree} alt="A ferarri" />
-          <img src={carFour} alt="A fast car" />
-          <img src={carFour} alt="A fast car" />
-          <img src={carThree} alt="A ferarri" />
-          <img src={carFour} alt="A fast car" />
-          <img src={carFour} alt="A fast car" />
+    <article className="programsWrapper" id="programs">
+      <div className="heading">
+        <h2>Programs & Curriculum</h2>
+        <p>Nurturing Growth and Empowering Learners for a Bright Future</p>
+      </div>
+      <article className="wrapper">
+        <div className="carousel" ref={sliderRef}>
+          <img src={carThree} alt="" />
+          <img src={carFour} alt="" />
+          <img src={carFive} alt="" />
         </div>
-      </div>
-      <div className="space-50vh lightBG">
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium
-          quia ipsum ullam facere dignissimos quos quisquam. Pariatur excepturi
-          eveniet error sint similique. Cupiditate exercitationem quaerat harum
-          cumque fuga commodi ipsum nulla, tempora dignissimos error voluptates
-          nobis praesentium pariatur explicabo possimus?
-        </p>
-      </div>
-    </div>
+      </article>
+    </article>
   );
-}
+};
 
 export default App;
